846e8d288f6d87c499eeab2cf523028d86694e6e
Rob Warner <rwarner@grailbox.com>
2020-12-09 09:07:44 -0500

    feat: day 9 part 2

--- 2020/9/index.js
+++ 2020/9/index.js
@@ -10,6 +10,18 @@ const canSum = (preamble, num) => {
   return false;
 };
 
+const findRange = (arr) => {
+  if (arr.length) {
+    let smallest = arr[0],
+      largest = arr[0];
+    for (const item of arr) {
+      if (item < smallest) smallest = item;
+      if (item > largest) largest = item;
+    }
+    return { smallest, largest };
+  }
+};
+
 const part1 = (nums, pLen) => {
   for (let i = 0, n = nums.length - pLen; i < n; i++) {
     const preamble = nums.slice(i, i + pLen);
@@ -18,7 +30,20 @@ const part1 = (nums, pLen) => {
   }
 };
 
-const part2 = (nums, pLen) => {};
+const part2 = (nums, bad) => {
+  const len = nums.length;
+  for (let i = 0; i < len - 1; i++) {
+    let accum = 0;
+    for (let j = i + 1; j < len; j++) {
+      accum += nums[j];
+      if (nums[i] + accum === bad) {
+        const { smallest, largest } = findRange(nums.slice(i, j + 1));
+        return smallest + largest;
+      }
+      if (nums[i] + accum > bad) break;
+    }
+  }
+};
 
 const main = () => {
   const data = fs.readFileSync("input", "utf-8");
@@ -27,8 +52,9 @@ const main = () => {
     .filter((el) => el.length > 0)
     .map((el) => +el);
 
-  console.log(part1(nums, 25));
-  console.log(part2(nums, 25));
+  const bad = part1(nums, 25);
+  console.log(bad);
+  console.log(part2(nums, bad));
 };
 
 main();
