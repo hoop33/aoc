c8c51df887ff2db24e51f5bd7095e7ecd14b7078
Rob Warner <rwarner@grailbox.com>
2020-12-11 13:04:27 -0500

    feat: day 11 part 2

--- 2020/11/index.js
+++ 2020/11/index.js
@@ -1,5 +1,16 @@
 const fs = require("fs");
 
+const DIRECTIONS = [
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+  [0, 1],
+  [-1, 1],
+  [-1, 0],
+];
+
 const getCountOfType = (grid, c) => {
   let count = 0;
   for (const row of grid) {
@@ -12,37 +23,51 @@ const getCountOfType = (grid, c) => {
 
 const getAdjacentOccupied = (grid, row, column) => {
   let num = 0;
-  for (
-    let cr = Math.max(0, row - 1);
-    cr <= Math.min(grid.length - 1, row + 1);
-    cr++
-  ) {
-    for (
-      let cc = Math.max(0, column - 1);
-      cc <= Math.min(grid[row].length - 1, column + 1);
-      cc++
-    ) {
-      if (!(cr === row && cc === column) && grid[cr][cc] === "#") num++;
+  for (const dir of DIRECTIONS) {
+    const x = column + dir[0];
+    const y = row + dir[1];
+    if (
+      0 <= x &&
+      x < grid[row].length &&
+      0 <= y &&
+      y < grid.length &&
+      grid[y][x] === "#"
+    )
+      num++;
+  }
+  return num;
+};
+
+const getLineOfSightOccupied = (grid, row, column) => {
+  const maxRow = grid.length;
+  const maxCol = grid[row].length;
+
+  let num = 0;
+  for (const dir of DIRECTIONS) {
+    const [x, y] = dir;
+    let cc = column + x;
+    let cr = row + y;
+    let seat = ".";
+    while (0 <= cc && cc < maxCol && 0 <= cr && cr < maxRow && seat === ".") {
+      seat = grid[cr][cc];
+      if (seat === "#") num++;
+      cc += x;
+      cr += y;
     }
   }
   return num;
 };
 
 const diff = (a, b) => {
-  if (a.length !== b.length) return true;
-
   for (let row = 0; row < a.length; row++) {
-    if (a[row].length !== b[row].length) return true;
-
     for (let column = 0; column < a[row].length; column++) {
       if (a[row][column] !== b[row][column]) return true;
     }
   }
-
   return false;
 };
 
-const iterate = (grid) => {
+const iterate = (grid, minOccupied, fn) => {
   const newGrid = [];
   for (let row = 0; row < grid.length; row++) {
     const newRow = [];
@@ -52,20 +77,16 @@ const iterate = (grid) => {
           newRow.push(".");
           break;
         case "L":
-          newRow.push(getAdjacentOccupied(grid, row, column) === 0 ? "#" : "L");
+          newRow.push(fn(grid, row, column) === 0 ? "#" : "L");
           break;
         case "#":
-          newRow.push(getAdjacentOccupied(grid, row, column) >= 4 ? "L" : "#");
+          newRow.push(fn(grid, row, column) >= minOccupied ? "L" : "#");
           break;
       }
     }
     newGrid.push(newRow);
   }
   return newGrid;
-};
-
-const stringify = (grid) => {
-  return grid.join("\n").replace(",", "");
 };
 
 const part1 = (lines) => {
@@ -76,30 +97,31 @@ const part1 = (lines) => {
 
   let different = true;
   while (different) {
-    let newGrid = iterate(grid);
+    let newGrid = iterate(grid, 4, getAdjacentOccupied);
     different = diff(newGrid, grid);
     grid = newGrid;
   }
   return getCountOfType(grid, "#");
 };
 
-const part2 = (lines) => {};
+const part2 = (lines) => {
+  let grid = [];
+  for (const line of lines) {
+    grid.push(line.split(""));
+  }
+
+  let different = true;
+  while (different) {
+    let newGrid = iterate(grid, 5, getLineOfSightOccupied);
+    different = diff(newGrid, grid);
+    grid = newGrid;
+  }
+  return getCountOfType(grid, "#");
+};
 
 const main = () => {
   const data = fs.readFileSync("input", "utf-8");
   const lines = data.split(/\r?\n/).filter((el) => el.length > 0);
-  //const lines = `L.LL.LL.LL
-  //LLLLLLL.LL
-  //L.L.L..L..
-  //LLLL.LL.LL
-  //L.LL.LL.LL
-  //L.LLLLL.LL
-  //..L.L.....
-  //LLLLLLLLLL
-  //L.LLLLLL.L
-  //L.LLLLL.LL`
-  //.split(/\r?\n/)
-  //.filter((el) => el.length > 0);
 
   console.log(part1(lines));
   console.log(part2(lines));
