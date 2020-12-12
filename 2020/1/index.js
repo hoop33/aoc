fe442caae8d03cb7d5c19dba7b12b920531498a4
Rob Warner <rwarner@grailbox.com>
2020-12-12 11:45:43 -0500

    feat: add tests

--- 2020/1/index.js
+++ 2020/1/index.js
@@ -1,3 +1,4 @@
+const assert = require("assert");
 const fs = require("fs");
 
 const part1 = (lines) => {
@@ -31,12 +32,28 @@ const part2 = (lines) => {
   }
 };
 
-const main = () => {
-  const data = fs.readFileSync("input", "utf-8");
-  const lines = data.split(/\r?\n/).filter((el) => el.length > 0);
-
-  console.log(part1(lines));
-  console.log(part2(lines));
+const test = (data, expected1, expected2) => {
+  assert.equal(part1(parseData(data)), expected1);
+  assert.equal(part2(parseData(data)), expected2);
 };
 
+const parseData = (data) => data.split(/\r?\n/).filter((el) => el.length > 0);
+
+const main = () => {
+  const data = fs.readFileSync("input", "utf-8");
+
+  console.log(part1(parseData(data)));
+  console.log(part2(parseData(data)));
+};
+
+test(
+  `1721
+979
+366
+299
+675
+1456`,
+  514579,
+  241861950
+);
 main();
